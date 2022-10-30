c4d6f244bbd40b2281e69d2af86af8ef1a46c3e1
Alexandre Quiblier <alexandre.quiblier@meona.de>
2022-10-30 14:10:41 +0100

    Updates

---
+++ precache-manifest.7af9fd9df333774be7e88a046a4f382a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce2e281609be7b572840937342e9873",
+    "url": "/MintableTokenCrowdSale/index.html"
+  },
+  {
+    "revision": "67beba18aef97d1b6465",
+    "url": "/MintableTokenCrowdSale/static/css/2.9c0acd10.chunk.css"
+  },
+  {
+    "revision": "67bb60dcb7a0cd94fcd5",
+    "url": "/MintableTokenCrowdSale/static/css/main.0e47b665.chunk.css"
+  },
+  {
+    "revision": "67beba18aef97d1b6465",
+    "url": "/MintableTokenCrowdSale/static/js/2.3dc9eab6.chunk.js"
+  },
+  {
+    "revision": "67bb60dcb7a0cd94fcd5",
+    "url": "/MintableTokenCrowdSale/static/js/main.d4db4619.chunk.js"
+  },
+  {
+    "revision": "cc5de73e8bf73cb6076a",
+    "url": "/MintableTokenCrowdSale/static/js/runtime-main.be5191a7.js"
+  }
+]);
